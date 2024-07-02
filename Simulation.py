f598d74faca1786965640dbf47a3ba67147996f6
Zeus <zeusofolympus1000@gmail.com>
2024-07-02 19:53:51 -0400

    saving once more

--- Simulation.py
+++ Simulation.py
@@ -1,5 +1,5 @@
 import json
-
+import uuid
 
 ACCOUNT=1000000
 SPLIT=5
@@ -23,13 +23,21 @@ ACCOUNT-=ACCOUNT
 INSURANCE_COST=(DAILY_RENT*(365*OCCUPANCY_RATE))*0.03
 MANAGEMENT_COST=5000
 CASH_DEDUCTION=0.05
+
+
+_houses=[]
+_details=[]
+
 details={
     "Account":ACCOUNT,
     "Properties":PROPERTIES,
     "Equity":EQUITY,
     "YearlyIncome":0,
-    "Cash":0
+    "Cash":0,
+    "Year":0,
+    "TotalDebt":MORTGAGE*SPLIT
 }
+
 new_house={
     "Downpayment":DOWN_PAYMENT,
     'YearlyMortgagePayment':YEARLY_MORTGAGE_PAYMENT,
@@ -44,21 +52,28 @@ new_house={
     "UtilityCost":UTILS,
     "InsuranceCost":INSURANCE_COST,
     "ManagementCost":MANAGEMENT_COST,
+    "Id":str(uuid.uuid4()),
+    "Year":0
 }
+for i in range(0, SPLIT):
+    _new_house=new_house.copy()
+    _new_house['Id']=str(uuid.uuid4())
+    _houses.append(_new_house)
 
-houses = [new_house.copy() for _ in range(SPLIT)]
 
 year=1
-for i in range(0,10):
-    print("Account", json.dumps(details, indent=4))
-    print("Example House", json.dumps(houses[0], indent=4))
-    print()
+for i in range(0,15):
+
+    details['Year']+=1
+    _details.append(details.copy())
 
     startingBalance=details['Account']
-    for house in houses:
+    for house in _houses:
         house['YearsPaid']+=1
+        house['Year']+=1
         details['Equity']+=MORTGAGE/30
         details['Account']+=(house['DailyRent']*(house['Occupancy']*365))-house['Taxes']-house['MaintenanceCost']-house['UtilityCost']-house['CleaningCost']-house['InsuranceCost']-house['ManagementCost']-house['YearlyMortgagePayment']
+        details['TotalDebt']-=MORTGAGE/30
 
     details['YearlyIncome']=details['Account']-startingBalance
     
@@ -68,13 +83,17 @@ for i in range(0,10):
     new_houses=int((details['Account'])/(HOUSE_VALUE/5))
 
     for i in range(0, new_houses):
-        houses.append(new_house)
+        _new_house=new_house.copy()
+        _new_house['Id']=str(uuid.uuid4())
+        _houses.append(_new_house)
         details['Account']-=(HOUSE_VALUE/5)
         details['Properties']+=1
+        details['TotalDebt']+=MORTGAGE
 
 
 
-
+print("Account", json.dumps(_details, indent=4))
+# print("Houses", json.dumps(_houses, indent=4))
 
 
 
